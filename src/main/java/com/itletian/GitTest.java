ba16cecbe73c85d35974528edd729877ee63d60d
wangletian <wangletian@7777777.com>
2023-03-11 20:55:13 +0800

    third commit

--- src/main/java/com/itletian/GitTest.java
+++ src/main/java/com/itletian/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello! git");
         System.out.println("我修改了一次");
+        System.out.println("我又修改了一次");
     }
 }
